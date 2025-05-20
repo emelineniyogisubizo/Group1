43ad5511b58d83675b9a325a870f135d2b3e4f50
Bright Joy Karanja <brightjoykaranja@gmail.com>
2025-05-20 13:13:09 +0300

    The added js file and the py file

---
+++ question1/index.py
@@ -0,0 +1,13 @@
+ def busyPeriods(employees, locations, capacity):
+    for location in locations:
+        for employee in employees:
+            employee == location
+
+    if len(employee) < capacity:
+        print("We need more employees")   
+    else:
+        print("Employees are enough") 
+
+def labour(hoursWorked, salary, lawLabourHours):
+    if hoursWorked > lawLabourHours:
+            
